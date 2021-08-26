178f662bb6a55287ca8935fb668a120de6f5294f
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-26 21:07:16 +0700

    GetX Show Dialog

--- flutter_getx/lib/screens/screen_1.dart
+++ flutter_getx/lib/screens/screen_1.dart
@@ -6,28 +6,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () => Get.toNamed(RoutesNamed.product_page),
-                child: Text("All Product >>")),
-          ],
+        appBar: AppBar(
+          title: Text("Home"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text("Show"),
-        onPressed: () => Get.snackbar("Test", "Test Snackbar",
-            duration: Duration(seconds: 3),
-            animationDuration: Duration(milliseconds: 300),
-            backgroundColor: Colors.amber,
-            backgroundGradient:
-                LinearGradient(colors: [Colors.blue, Colors.green])),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () => Get.toNamed(RoutesNamed.product_page),
+                  child: Text("All Product >>")),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              child: Text("Show"),
+              onPressed: () => Get.snackbar("Test", "Test Snackbar",
+                  duration: Duration(seconds: 3),
+                  animationDuration: Duration(milliseconds: 300),
+                  backgroundColor: Colors.amber,
+                  backgroundGradient:
+                      LinearGradient(colors: [Colors.blue, Colors.green])),
+            ),
+            FloatingActionButton(
+                child: Text("Dialog"),
+                onPressed: () => Get.defaultDialog(
+                    title: "Hello", middleText: "Ini Tester",
+                    textConfirm: "Ok",
+                    onConfirm: () => print('ok'),
+                    textCancel: "Batal",
+                    onCancel: ()=> print('keluar'),
+                    
+                    )),
+          ],
+        ));
   }
 }
