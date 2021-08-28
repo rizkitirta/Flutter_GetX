393d87cdaa72566c641dc2110f210216d5a8ad53
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-28 16:24:41 +0700

    GetX Other Advance APIs

--- flutter_getx/lib/main.dart
+++ flutter_getx/lib/main.dart
@@ -15,8 +15,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: HomePage(),
+      home: PageSatu(),
       getPages: AppPages.pages,
     );
+  }
+}
+
+class PageSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Page Satu"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () =>
+                      Get.to(() => PageDua(), arguments: "From page 1"),
+                  child: Text("Page 2 >>")),
+            ],
+          ),
+        ));
+  }
+}
+
+class PageDua extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Page Dua"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("${Get.arguments}"),
+              ElevatedButton(onPressed: () => Get.to(() => PageTiga()), child: Text("Page 3 >>")),
+            ],
+          ),
+        ));
+  }
+}
+
+class PageTiga extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Page Tiga"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text("${Get.previousRoute}"),
+              ElevatedButton(onPressed: () {}, child: Text("Page 2 >>")),
+            ],
+          ),
+        ));
   }
 }
